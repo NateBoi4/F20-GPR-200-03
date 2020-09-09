745074af5961b0d1023510c9e21aa28cdf522988
Nathan Boisvert <nathan.boisvert@mymail.champlain.edu>
2020-09-09 15:11:38 -0400

    Forgot to save small change

--- GPRO-Graphics1/include/gpro/ray_tracing.h
+++ GPRO-Graphics1/include/gpro/ray_tracing.h
@@ -11,9 +11,9 @@ public:
     vec3 origin() const { return orig; }
     vec3 direction() const { return dir; }
 
-    vec3 at(double t) const {
-        return orig + t * dir;
-    }
+    //vec3 at(double t) const {
+    //    return orig + t * dir;
+    //}
 
 public:
     vec3 orig;
