1b52cbaf3f2b92afeba4b178a2326944ad56aee0
Nathan Boisvert <nathan.boisvert@mymail.champlain.edu>
2020-09-09 13:17:13 -0400

    Switched output process to make use of vectors

--- GPRO-Graphics1/source/image.cpp
+++ GPRO-Graphics1/source/image.cpp
@@ -16,6 +16,7 @@ Work references Chapter 2: Output an Image
 
 using namespace std;
 
+#include "gpro/gpro-math/gproVector.h"
 #include "gpro/image.h"
 
 void image() {
@@ -24,20 +25,28 @@ void image() {
 	const int width = 400; //Image width
 	const int height = 644; //Image height
 
-	//Create Image
-    cout << "P3\n" << width << ' ' << height << "\n255\n";
+	//Create Image by writing to file
+    cout << "P3\n" << width << ' ' << height << "\n255\n"; 
+    //P3 for ASCII colors, image width, image height, and 255 is max color on the rgb scale, 0 to 255
 
-    for (double i = height - 1; i >= 0; --i) {
+    float3 colors; //Create float vector named colors
+
+    /*
+    Using a for loop and a nested for loop, this code creates the image by writing to the program output.
+    It writes in terms a vec3 which determines the colors of each row of pixels.
+    It moves across the rows then down the columns as it writes each color of the image
+    The program ends by informing the user the process is complete by writing to the error log.
+    */
+    for (double i = height - 1.0; i >= 0; --i) {
         for (double j = 0; j < width; ++j) {
-            double r = j / (width - 1);
-            double g = i / (height - 1);
-            double b = 0.50;
+            vec3init(colors, float((j / (width - 1.0))), float((i / (height - 1.0))), float(0.5));
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            int r = static_cast<int>(255.999 * colors[0]);
+            int g = static_cast<int>(255.999 * colors[1]);
+            int b = static_cast<int>(255.999 * colors[2]);
 
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            cout << r << ' ' << g << ' ' << b << '\n';
         }
     }
+    cerr << "Done\n";
 }
